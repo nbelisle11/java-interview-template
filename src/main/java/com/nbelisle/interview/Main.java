4dcb08f4efc378880facd536b322e19b8d343067
Nate Belisle <nbelisle11@gmail.com>
2023-07-05 09:33:35 -0400

    first commit

---
+++ src/main/java/com/nbelisle/interview/Main.java
@@ -0,0 +1,10 @@
+package com.nbelisle.interview;
+
+public class Main {
+
+  private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(Main.class);
+
+  public static void main(String... args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ src/test/java/com/nbelisle/interview/MainTest.java
@@ -0,0 +1,15 @@
+package com.nbelisle.interview;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  void testStub() {
+    assertThat(2).isEqualTo(2);
+  }
+}
